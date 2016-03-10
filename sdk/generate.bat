a9d03cde05f4881523f54e49a12b9ef495a18f6c
Wallace Breza <wabreza@microsoft.com>
2016-03-10 15:59:53 -0800

    Add swagger.json that will be used for public preview

--- sdk/generate.bat
+++ sdk/generate.bat
@@ -2,6 +2,6 @@ pushd "%~dp0"
 rem rm -rf Generated\v1.0
 rem AutoRest.exe -CodeGenerator CSharp -Modeler Swagger -Input https://onebox-redirect.analysis.windows-int.net/swagger/docs/v1.0 -Namespace Microsoft.PowerBI.Api -output PowerBI.Api\Source -name PowerBIClient -AddCredentials
 rm -rf Generated\Beta
-AutoRest.exe -CodeGenerator CSharp -Modeler Swagger -Input c:\users\wabreza\desktop\swagger.json -Namespace Microsoft.PowerBI.Api.Beta -output PowerBI.Api\Source\Beta -name PowerBIClient -AddCredentials
+AutoRest.exe -CodeGenerator CSharp -Modeler Swagger -Input swagger.json -Namespace Microsoft.PowerBI.Api.Beta -output PowerBI.Api\Source\Beta -name PowerBIClient -AddCredentials
 popd
 pause
