b1da6d745d0beaa5149e8ee9beb34de5176b2737
Ali Hamud <alihamud@microsoft.com>
2016-09-08 10:17:33 +0300

    Adding a windows forms sample

---
+++ samples/winforms/WindowsFormsSample/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace WindowsFormsSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.API = new System.Windows.Forms.Label();
+            this.txtBaseUrl = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAccessKey = new System.Windows.Forms.TextBox();
+            this.lblCollection = new System.Windows.Forms.Label();
+            this.txtCollection = new System.Windows.Forms.TextBox();
+            this.lblWorkspaceId = new System.Windows.Forms.Label();
+            this.txtWorkspaceId = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtReports = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRequestedReportId = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.flowLayoutPanel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.flowLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(837, 550);
+            this.splitContainer1.SplitterDistance = 259;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.API);
+            this.flowLayoutPanel2.Controls.Add(this.txtBaseUrl);
+            this.flowLayoutPanel2.Controls.Add(this.label4);
+            this.flowLayoutPanel2.Controls.Add(this.txtAccessKey);
+            this.flowLayoutPanel2.Controls.Add(this.lblCollection);
+            this.flowLayoutPanel2.Controls.Add(this.txtCollection);
+            this.flowLayoutPanel2.Controls.Add(this.lblWorkspaceId);
+            this.flowLayoutPanel2.Controls.Add(this.txtWorkspaceId);
+            this.flowLayoutPanel2.Controls.Add(this.btnGenerate);
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Controls.Add(this.txtReports);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(259, 550);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // API
+            // 
+            this.API.AutoSize = true;
+            this.API.Location = new System.Drawing.Point(3, 5);
+            this.API.Name = "API";
+            this.API.Size = new System.Drawing.Size(44, 13);
+            this.API.TabIndex = 0;
+            this.API.Text = "base url";
+            // 
+            // txtBaseUrl
+            // 
+            this.txtBaseUrl.Location = new System.Drawing.Point(3, 21);
+            this.txtBaseUrl.Name = "txtBaseUrl";
+            this.txtBaseUrl.Size = new System.Drawing.Size(253, 20);
+            this.txtBaseUrl.TabIndex = 1;
+            this.txtBaseUrl.Text = "https://api.powerbi.com";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "access key";
+            // 
+            // txtAccessKey
+            // 
+            this.txtAccessKey.Location = new System.Drawing.Point(3, 60);
+            this.txtAccessKey.Name = "txtAccessKey";
+            this.txtAccessKey.Size = new System.Drawing.Size(253, 20);
+            this.txtAccessKey.TabIndex = 3;
+            // 
+            // lblCollection
+            // 
+            this.lblCollection.AutoSize = true;
+            this.lblCollection.Location = new System.Drawing.Point(3, 83);
+            this.lblCollection.Name = "lblCollection";
+            this.lblCollection.Size = new System.Drawing.Size(52, 13);
+            this.lblCollection.TabIndex = 4;
+            this.lblCollection.Text = "collection";
+            // 
+            // txtCollection
+            // 
+            this.txtCollection.Location = new System.Drawing.Point(3, 99);
+            this.txtCollection.Name = "txtCollection";
+            this.txtCollection.Size = new System.Drawing.Size(253, 20);
+            this.txtCollection.TabIndex = 5;
+            // 
+            // lblWorkspaceId
+            // 
+            this.lblWorkspaceId.AutoSize = true;
+            this.lblWorkspaceId.Location = new System.Drawing.Point(3, 122);
+            this.lblWorkspaceId.Name = "lblWorkspaceId";
+            this.lblWorkspaceId.Size = new System.Drawing.Size(70, 13);
+            this.lblWorkspaceId.TabIndex = 6;
+            this.lblWorkspaceId.Text = "workspace id";
+            // 
+            // txtWorkspaceId
+            // 
+            this.txtWorkspaceId.Location = new System.Drawing.Point(3, 138);
+            this.txtWorkspaceId.Name = "txtWorkspaceId";
+            this.txtWorkspaceId.Size = new System.Drawing.Size(253, 20);
+            this.txtWorkspaceId.TabIndex = 7;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnGenerate.Location = new System.Drawing.Point(148, 164);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(108, 23);
+            this.btnGenerate.TabIndex = 8;
+            this.btnGenerate.Text = "Get Reports";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 190);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 50, 0, 0);
+            this.label2.Size = new System.Drawing.Size(44, 63);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Reports";
+            // 
+            // txtReports
+            // 
+            this.txtReports.Location = new System.Drawing.Point(3, 256);
+            this.txtReports.Multiline = true;
+            this.txtReports.Name = "txtReports";
+            this.txtReports.Size = new System.Drawing.Size(253, 287);
+            this.txtReports.TabIndex = 10;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.txtRequestedReportId);
+            this.flowLayoutPanel1.Controls.Add(this.btnSubmit);
+            this.flowLayoutPanel1.Controls.Add(this.webBrowser1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(574, 550);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Report Id";
+            // 
+            // txtRequestedReportId
+            // 
+            this.txtRequestedReportId.Location = new System.Drawing.Point(3, 21);
+            this.txtRequestedReportId.Name = "txtRequestedReportId";
+            this.txtRequestedReportId.Size = new System.Drawing.Size(485, 20);
+            this.txtRequestedReportId.TabIndex = 0;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(3, 47);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(3, 76);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(559, 467);
+            this.webBrowser1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 550);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Windows Forms Sample";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label API;
+        private System.Windows.Forms.TextBox txtBaseUrl;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAccessKey;
+        private System.Windows.Forms.Label lblCollection;
+        private System.Windows.Forms.TextBox txtCollection;
+        private System.Windows.Forms.Label lblWorkspaceId;
+        private System.Windows.Forms.TextBox txtWorkspaceId;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRequestedReportId;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtReports;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ samples/winforms/WindowsFormsSample/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.PowerBI.Api.V1;
+using Microsoft.PowerBI.Api.V1.Models;
+using Microsoft.PowerBI.Security;
+using Microsoft.Rest;
+
+namespace WindowsFormsSample
+{
+    public partial class Form1 : Form
+    {
+        private IList<Report> Reports { get; set; }
+
+        private string AccessToken { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Create a token credentials with "AppKey" type
+            var credentials = new TokenCredentials(txtAccessKey.Text, "AppKey");
+
+            using (var client = new PowerBIClient(credentials))
+            {
+                client.BaseUri = new Uri(txtBaseUrl.Text);
+
+                var reportsResponse = client.Reports.GetReports(txtCollection.Text, txtWorkspaceId.Text);
+
+                Reports = reportsResponse.Value;
+
+                Print(Reports);
+            }
+        }
+
+        private void Print(IList<Report> reports)
+        {
+            txtReports.Text = string.Empty;
+            foreach (var report in reports)
+            {
+                txtReports.Text += report.Name + Environment.NewLine + report.Id + Environment.NewLine + Environment.NewLine;
+            }
+
+            // Put the last report Id in the request textbox.
+            var lastReport = reports.LastOrDefault();
+            if (lastReport != null)
+            {
+                txtRequestedReportId.Text = lastReport.Id;
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            var report = FindReport(txtRequestedReportId.Text);
+
+            if (report == null)
+            {
+                MessageBox.Show("No such report");
+                return;
+            }
+
+            var embedToken = PowerBIToken.CreateReportEmbedToken(txtCollection.Text, txtWorkspaceId.Text, report.Id);
+
+            AccessToken = embedToken.Generate(txtAccessKey.Text);
+
+            webBrowser1.DocumentCompleted += WebBrowser1_DocumentCompleted;
+
+            webBrowser1.Navigate(report.EmbedUrl);
+
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (AccessToken != null)
+            {
+                var message = "{\"action\":\"loadReport\",\"accessToken\":\"" + AccessToken + "\"}";
+                webBrowser1.Document.InvokeScript("postMessage", new object[] { message, "*" });
+            }
+            else
+            {
+                MessageBox.Show("AccessToken is not defined.");
+            }
+        }
+
+        private Report FindReport(string reportId)
+        {
+            if (Reports != null)
+            {
+                foreach (var report in Reports)
+                {
+                    if (report.Id == reportId)
+                    {
+                        return report;
+                    }
+                }
+                
+            }
+
+            MessageBox.Show("You should get a list of reports first. click Generate");
+            return null;
+        }
+    }
+}
