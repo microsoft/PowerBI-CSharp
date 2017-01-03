dfefbcad184e680159f99642d480f110f9c56f55
t-yoigra <t-yoigra@T-YOIGRA-DEV2>
2017-01-03 00:37:35 -0800

    embed-a-dashboard-into-an-app Adding supoort SovarCloud

--- samples/webforms/embed-a-dashboard-into-an-app/PBIWebApp/Properties/Settings.Designer.cs
+++ samples/webforms/embed-a-dashboard-into-an-app/PBIWebApp/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace PBIWebApp.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("{Enter your app ClientID}")]
         public string ClientID {
             get {
                 return ((string)(this["ClientID"]));
@@ -34,10 +34,37 @@ namespace PBIWebApp.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("{Enter your app SecretKey}")]
         public string ClientSecret {
             get {
                 return ((string)(this["ClientSecret"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://login.windows.net/common/oauth2/authorize/")]
+        public string AADAuthorityUri {
+            get {
+                return ((string)(this["AADAuthorityUri"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://analysis.windows.net/powerbi/api")]
+        public string PowerBiAPI {
+            get {
+                return ((string)(this["PowerBiAPI"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://api.powerbi.com/v1.0/myorg/")]
+        public string PowerBiDataset {
+            get {
+                return ((string)(this["PowerBiDataset"]));
             }
         }
     }
