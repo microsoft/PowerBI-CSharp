ab369e42522c1fdbd062df2b732985bc5fa70972
Ali Hamud <alihamud@microsoft.com>
2017-11-12 14:25:32 +0200

    add swagger to csharp repo

--- sdk/generateV1.bat
+++ sdk/generateV1.bat
@@ -1,5 +1,5 @@
 pushd "%~dp0"
 del PowerBI.Api\Source\V1\
-AutoRest -CodeGenerator CSharp -Input https://raw.githubusercontent.com/Microsoft/powerbi-rest-api-specs/master/swagger.json -Namespace Microsoft.PowerBI.Api.V1 -OutputDirectory PowerBI.Api\Source\V1\ -ClientName PowerBIClient -AddCredentials true
+AutoRest -CodeGenerator CSharp -Input "./swaggers/swagger.json" -Namespace Microsoft.PowerBI.Api.V1 -OutputDirectory PowerBI.Api\Source\V1\ -ClientName PowerBIClient -AddCredentials true
 popd
 pause
