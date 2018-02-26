bb6e3bd57cd570706bb9af5876f51ba5c3f3276f
Ran Didi <randidi@microsoft.com>
2018-02-26 07:23:18 +0000

    Merged PR 3989: Merge fixConstructorsAmbiguity to dev
    Fix ambiguous constructors for extended models

--- sdk/PowerBI.Api/Extensions/V2/Models/GenerateTokenRequest.cs
+++ sdk/PowerBI.Api/Extensions/V2/Models/GenerateTokenRequest.cs
@@ -13,13 +13,64 @@ namespace Microsoft.PowerBI.Api.V2.Models
         /// <param name="datasetId">The dataset Id</param>
         /// <param name="allowSaveAs">Allow SaveAs the report with generated
         /// token.</param>
-        /// <param name="identities">The effective identities to use for RLS
+        /// <param name="identity">The effective identity to use for RLS
         /// rules</param>
-        public GenerateTokenRequest(string accessLevel = default(string), string datasetId = default(string), bool? allowSaveAs = default(bool?), EffectiveIdentity identity = default(EffectiveIdentity))
+        public GenerateTokenRequest(string accessLevel, string datasetId, bool? allowSaveAs, EffectiveIdentity identity)
         {
             AccessLevel = accessLevel;
             DatasetId = datasetId;
             AllowSaveAs = allowSaveAs;
+            Identities = new[] { identity };
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the GenerateTokenRequest class.
+        /// </summary>
+        /// <param name="accessLevel">The dataset mode or type. Possible values
+        /// include: 'View', 'Edit', 'Create'</param>
+        /// <param name="allowSaveAs">Allow SaveAs the report with generated
+        /// token.</param>
+        /// <param name="identity">The effective identity to use for RLS
+        /// rules</param>
+        public GenerateTokenRequest(string accessLevel, bool? allowSaveAs, EffectiveIdentity identity)
+        {
+            AccessLevel = accessLevel;
+            DatasetId = default(string);
+            AllowSaveAs = allowSaveAs;
+            Identities = new[] { identity };
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the GenerateTokenRequest class.
+        /// </summary>
+        /// <param name="accessLevel">The dataset mode or type. Possible values
+        /// include: 'View', 'Edit', 'Create'</param>
+        /// <param name="datasetId">The dataset Id</param>
+        /// <param name="identity">The effective identity to use for RLS
+        /// rules</param>
+        public GenerateTokenRequest(string accessLevel, string datasetId, EffectiveIdentity identity)
+        {
+            AccessLevel = accessLevel;
+            DatasetId = datasetId;
+            AllowSaveAs = null;
+            Identities = new[] { identity };
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the GenerateTokenRequest class.
+        /// </summary>
+        /// <param name="accessLevel">The dataset mode or type. Possible values
+        /// include: 'View', 'Edit', 'Create'</param>
+        /// <param name="identity">The effective identity to use for RLS
+        /// rules</param>
+        public GenerateTokenRequest(string accessLevel, EffectiveIdentity identity)
+        {
+            AccessLevel = accessLevel;
+            DatasetId = default(string);
+            AllowSaveAs = null;
             Identities = new[] { identity };
             CustomInit();
         }
