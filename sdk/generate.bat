150e511d4e829d9ba600b2aa4fb474ce6d5d0a12
Ran Didi <randidi@microsoft.com>
2018-07-17 08:39:39 +0000

    Merged PR 7362: Fix AutoRest to use hardcoded C# extension version
    Fix AutoRest to use hardcoded C# extension version

--- sdk/generate.bat
+++ sdk/generate.bat
@@ -2,10 +2,7 @@ pushd "%~dp0"
 del PowerBI.Api\Source\V2\Models
 del PowerBI.Api\Source\V2\
 
-set swagger="%1"
-IF "%~1"=="" set swagger="./swaggers/swaggerV2.json"
-
-@echo Generating code using %swagger%
-AutoRest -CodeGenerator CSharp -Input %swagger% -Namespace Microsoft.PowerBI.Api.V2 -OutputDirectory PowerBI.Api\Source\V2\ -ClientName PowerBIClient -AddCredentials true
+@echo Generating code using powerbi.md
+AutoRest powerbi.md
 popd
 pause
