0c4eb4a78ba22cb2f1f2af695525775b2b288143
Mahir Diab <madia@microsoft.com>
2019-05-20 19:09:53 +0000

    Merged PR 29533: Revert "Merged PR 28348: updated CreateGroup for defaultly create workspace v2"
    Revert "Merged PR 28348: updated CreateGroup for defaultly create workspace v2"
    
    This reverts commit ceee1e357f282cb38db0389825b2c91e64ea28aa.

--- sdk/PowerBI.Api/Source/V2/IGroupsOperations.cs
+++ sdk/PowerBI.Api/Source/V2/IGroupsOperations.cs
@@ -63,7 +63,7 @@ namespace Microsoft.PowerBI.Api.V2
         /// Create group request parameters
         /// </param>
         /// <param name='workspaceV2'>
-        /// Determines whether to create a Workspace V2. The default value is
+        /// Preview feature: Create a workspace V2. The only supported value is
         /// true.
         /// </param>
         /// <param name='customHeaders'>
