e19583d0026a8c426136c688e2f6dd7101a22ca2
Laurent Amar <laurenta@microsoft.com>
2021-09-12 08:01:24 +0000

    Merged PR 201698: Increment Patch version
    Increment Patch version

--- scripts/pack-sdk.cmd
+++ scripts/pack-sdk.cmd
@@ -6,13 +6,15 @@ call .\init-dev-cmd.cmd
 REM Set Major and Minor package version
 SET MAJOR=%CDP_MAJOR_NUMBER_ONLY%
 SET MINOR=%CDP_MINOR_NUMBER_ONLY%
+SET PATCH=1
 if "%MAJOR%"=="" SET MAJOR=0
 if "%MINOR%"=="" SET MINOR=0
+
 
 echo ========================================
 echo "Pack PowerBI.Api.csproj Release - AnyCPU..."
 echo ========================================
-call msbuild %~dp0..\sdk\PowerBI.Api\PowerBI.Api.csproj /t:pack /p:Configuration=Release /p:CdpxPostSigning=true /p:PackageVersion=%MAJOR%.%MINOR%.0 /p:PackageOutputPath=%~dp0..\pack\Clean
+call msbuild %~dp0..\sdk\PowerBI.Api\PowerBI.Api.csproj /t:pack /p:Configuration=Release /p:CdpxPostSigning=true /p:PackageVersion=%MAJOR%.%MINOR%.%PATCH% /p:PackageOutputPath=%~dp0..\pack\Clean
 
 set EX=%ERRORLEVEL%
 
