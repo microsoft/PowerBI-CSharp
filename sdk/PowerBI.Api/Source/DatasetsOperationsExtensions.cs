580020dd38d5343d0131d24f76fe7de9ff25317e
Megan Kamiya <mekam@microsoft.com>
2023-09-19 07:42:57 +0000

    Merged PR 449122: Update execute queries permissions docs
    Update the permissions needed to mention that you must have access to the workspace when using the Execute Queries in Group API.
    
    Related work items: #1188178

--- sdk/PowerBI.Api/Source/DatasetsOperationsExtensions.cs
+++ sdk/PowerBI.Api/Source/DatasetsOperationsExtensions.cs
@@ -4678,8 +4678,8 @@ namespace Microsoft.PowerBI.Api
             ///
             /// ## Permissions
             ///
-            /// The user must have dataset read permissions. For more information, see
-            /// [Manage dataset access
+            /// The user must have access to the workspace and dataset read permissions.
+            /// For more information, see [Manage dataset access
             /// permissions](/power-bi/connect-data/service-datasets-manage-access-permissions).
             ///
             /// ## Required Scope
@@ -4756,8 +4756,8 @@ namespace Microsoft.PowerBI.Api
             ///
             /// ## Permissions
             ///
-            /// The user must have dataset read permissions. For more information, see
-            /// [Manage dataset access
+            /// The user must have access to the workspace and dataset read permissions.
+            /// For more information, see [Manage dataset access
             /// permissions](/power-bi/connect-data/service-datasets-manage-access-permissions).
             ///
             /// ## Required Scope
